eab8d63fe0bebeccfc9c4a9de9a5dcd445ba6e45
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-08-24 07:29:12 +0700

    register page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+@section('main')
+    <div class="flex justify-center mt-6">
+        {{-- card --}}
+        <form action="{{ route('register') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="card card-compact w-fit bg-neutral shadow-xl flex items-center text-center">
+                <div class="card-body">
+                    <h2 class="font-bold text-2xl">Register</h2>
+
+                    <label for="profile_photo" class="label">Upload Your Photo Profile</label>
+                    <input type="file" id="profile_photo" name="profile_photo" class="file-input file-input-primary">
+                    @error('profile_photo')
+                        <p class="text-red-600">{{ $message }}</p>
+                    @enderror
+
+                    <label class="label">
+                        <span class="label-text">Enter Your Name</span>
+                    </label>
+                    <input type="text" name="name" id="name" placeholder="Input Your Name Here"
+                        class="input input-bordered input-primary w-96 max-w-xs" value="{{ old('name') }}" />
+                    @error('email')
+                        <span class="text-red-500">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                    <label class="label">
+                        <span class="label-text">Enter Email</span>
+                    </label>
+                    <input type="email" name="email" id="email" placeholder="Input Email Here"
+                        class="input input-bordered input-primary w-96 max-w-xs" value="{{ old('email') }}" />
+                    @error('email')
+                        <span class="text-red-500">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                    <label class="label">
+                        <span class="label-text">Enter Password</span>
+                    </label>
+                    <input type="password" name="password" id="password" placeholder="Input Password Here"
+                        class="input input-bordered input-primary w-96 max-w-xs" value="{{ old('password') }}" />
+                    @error('password')
+                        <span class="text-red-500">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                    <label class="label">
+                        <span class="label-text">Confirm Your Password</span>
+                    </label>
+                    <input type="password" name="password_confirmation" id="password_confirmation"
+                        placeholder="Input Password Confirmation Here"
+                        class="input input-bordered input-primary w-96 max-w-xs" />
+
+
+                    <button type="submit" class="btn btn-outline btn-success mt-3">Register</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
